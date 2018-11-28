2ce508c8d4cdce127ad3b703fab28211f33c4cd2
Momaritti <marie-theres.monz@student.design-akademie-berlin.de>
2018-11-29 00:37:34 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// module aliases
+var Engine = Matter.Engine,
+    //Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var box1;
+
+function setup() {
+    createCanvas(400, 400);
+    engine = Engine.create();
+    world = engine.world;
+    Engine.run(engine);
+    box1 = new Box(200, 100, 50, 50);
+}
+
+function draw() {
+    background(51);
+    box1.show();
+}
+
+
